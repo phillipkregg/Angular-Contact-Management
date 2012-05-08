f074bd4368b4550a7d70cfe104f5250b8cbeeaff
PhillipKregg <phillipkregg@gmail.com>
2012-05-08 18:27:54 -0500

    Applied dependency injection to Angular controllers

--- app/assets/javascripts/app/controllers/controllers.js
+++ app/assets/javascripts/app/controllers/controllers.js
@@ -4,6 +4,10 @@ function ContactListCtrl($scope, Contact) {
   $scope.contacts = Contact.query();  
 }
 
+ContactListCtrl.$inject = ['$scope', 'Contact'];
+
 function ContactDetailCtrl($scope, $routeParams, Contact) {
   $scope.contact = Contact.query({"contactId": contact.id});
 }
+
+ContactDetailCtrl.$inject = ['$scope', '$routeParams', 'Contact'];
