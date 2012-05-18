434b99edb499aa052360f6ae94d631a0ae6598b1
Your Name <phillipkregg@gmail.com>
2012-05-17 19:46:02 -0500

    Added a top navigation

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -5,8 +5,8 @@
 angular.module('contactapp', ['ngResource']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/contacts', {template: 'assets/app/partials/contact-list.html',   controller: ContactListCtrl}).
-      when('/contacts/new', {template: 'assets/app/partials/new-contact.html',   controller: ContactListCtrl}).
+      when('/contacts', {template: 'assets/app/partials/contact-list.html', controller: ContactListCtrl}).
+      when('/contacts/new', {template: 'assets/app/partials/new-contact.html', controller: ContactListCtrl}).
       when('/contacts/:contact_id', {template: 'assets/app/partials/contact-detail.html', controller: ContactDetailCtrl}).
       otherwise({redirectTo: '/contacts'});
 }]);
